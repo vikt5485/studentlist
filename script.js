f7df490b598b1b23a60a2e6f64951db8b7c52f19
viktor kjeldal <vikt5485@stud.kea.dk>
2020-02-08 18:28:36 +0100

    added theme selector + css styling to each house

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let students = [];
 const template = document.querySelector(".student-temp");
 const dest = document.querySelector(".student-list");
 const popup = document.querySelector(".popup");
+const wrapper = document.querySelector(".student-wrapper");
 
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
     console.log("start");
     getJson();
+    document.querySelector("select#theme").addEventListener("change", selectTheme);
 }
 
 async function getJson() {
@@ -21,39 +23,47 @@ async function getJson() {
     showStudents();
 }
 
+function selectTheme() {
+    document.querySelector("body").setAttribute("data-house", this.value);
+
+}
+
+
 function showStudents() {
     console.log("showStudents");
     students.forEach(student => {
         let klon = template.cloneNode(true).content;
 
         klon.querySelector("li").textContent = student.fullname;
+        if (student.fullname == "Harry Potter") {
+            klon.querySelector("li").textContent = student.fullname + " ⚡";
+        }
 
         dest.appendChild(klon);
 
         dest.lastElementChild.addEventListener("click", () => {
             showPopup(student);
-        })
-    })
+        });
+    });
 }
 
 function showPopup(student) {
     console.log("showPopup");
-    popup.style.display = "flex";
+    popup.classList.add("popup-appear");
+    wrapper.classList.add("wrapper-effect");
 
-    if (student.house == "Gryffindor") {
-        popup.style.backgroundColor = "rgba(100, 23, 59, 0.7)";
-    } else if (student.house == "Hufflepuff") {
-        popup.style.backgroundColor = "rgba(239, 180, 31, 0.7)";
-    } else if (student.house == "Ravenclaw") {
-        popup.style.backgroundColor = "rgba(1, 76, 126, 0.7)";
-    } else if (student.house == "Slytherin") {
-        popup.style.backgroundColor = "rgba(17, 69, 50, 0.7)";
-    }
+    document.querySelector(".popup-content").setAttribute("data-house", student.house);
 
     document.querySelector(".popup-content>h2").textContent = student.fullname;
-    document.querySelector(".popup-content>h3").textContent = "House: " + student.house;
+    document.querySelector(".popup-content>h3").textContent =
+        "House: " + student.house;
+
+    if (student.fullname == "Harry Potter") {
+        document.querySelector(".popup-content>h2").textContent = student.fullname + " ⚡";
+    }
 
     document.querySelector(".close").addEventListener("click", () => {
-        document.querySelector(".popup").style.display = "none";
-    })
+        popup.classList.remove("popup-appear");
+        wrapper.classList.remove("wrapper-effect");
+    });
 }
