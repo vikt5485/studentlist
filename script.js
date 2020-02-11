69bf1cc18bed24adfde76f15e12306e21cb497dd
viktor kjeldal <vikt5485@stud.kea.dk>
2020-02-11 09:46:35 +0100

    data houses toLowerCase()

--- script.js
+++ script.js
@@ -7,61 +7,70 @@ const wrapper = document.querySelector(".student-wrapper");
 document.addEventListener("DOMContentLoaded", start);
 
 function start() {
-    console.log("start");
-    getJson();
-    document.querySelector("select#theme").addEventListener("change", selectTheme);
+  console.log("start");
+  getJson();
+  document
+    .querySelector("select#theme")
+    .addEventListener("change", selectTheme);
 }
 
 async function getJson() {
-    console.log("getJson");
+  console.log("getJson");
 
-    const jsonData = await fetch("students1991.json");
+  const jsonData = await fetch("students1991.json");
 
-    students = await jsonData.json();
-    console.log(students);
+  students = await jsonData.json();
+  console.log(students);
 
-    showStudents();
+  showStudents();
 }
 
 function selectTheme() {
-    document.querySelector("body").setAttribute("data-house", this.value);
+  document
+    .querySelector("body")
+    .setAttribute("data-house", this.value.toLowerCase());
 }
 
 function showStudents() {
-    console.log("showStudents");
-    students.forEach(student => {
-        let klon = template.cloneNode(true).content;
+  console.log("showStudents");
+  students.forEach(showAStudent);
+}
 
-        klon.querySelector("li").textContent = student.fullname;
-        if (student.fullname == "Harry Potter") {
-            klon.querySelector("li").textContent = student.fullname + " ⚡";
-        }
+function showAStudent(student) {
+  let klon = template.cloneNode(true).content;
 
-        dest.appendChild(klon);
+  klon.querySelector("li").textContent = student.fullname;
+  if (student.fullname == "Harry Potter") {
+    klon.querySelector("li").textContent = student.fullname + " ⚡";
+  }
 
-        dest.lastElementChild.addEventListener("click", () => {
-            showPopup(student);
-        });
-    });
+  dest.appendChild(klon);
+
+  dest.lastElementChild.addEventListener("click", () => {
+    showPopup(student);
+  });
 }
 
 function showPopup(student) {
-    console.log("showPopup");
-    popup.classList.add("popup-appear");
-    wrapper.classList.add("wrapper-effect");
+  console.log("showPopup");
+  popup.classList.add("popup-appear");
+  wrapper.classList.add("wrapper-effect");
 
-    document.querySelector(".popup-content").setAttribute("data-house", student.house);
+  document
+    .querySelector(".popup-content")
+    .setAttribute("data-house", student.house.toLowerCase());
 
-    document.querySelector(".popup-content>h2").textContent = student.fullname;
-    document.querySelector(".popup-content>h3").textContent =
-        "House: " + student.house;
+  document.querySelector(".popup-content>h2").textContent = student.fullname;
+  document.querySelector(".popup-content>h3").textContent =
+    "House: " + student.house;
 
-    if (student.fullname == "Harry Potter") {
-        document.querySelector(".popup-content>h2").textContent = student.fullname + " ⚡";
-    }
+  if (student.fullname == "Harry Potter") {
+    document.querySelector(".popup-content>h2").textContent =
+      student.fullname + " ⚡";
+  }
 
-    document.querySelector(".close").addEventListener("click", () => {
-        popup.classList.remove("popup-appear");
-        wrapper.classList.remove("wrapper-effect");
-    });
+  document.querySelector(".close").addEventListener("click", () => {
+    popup.classList.remove("popup-appear");
+    wrapper.classList.remove("wrapper-effect");
+  });
 }
